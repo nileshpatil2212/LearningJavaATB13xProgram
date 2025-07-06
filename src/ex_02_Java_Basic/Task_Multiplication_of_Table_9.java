55b1a2a20bfa4e1039fca06b1668fd96f750a494
nileshpatil2212 <nilldpatil77@gmaail.com>
2025-07-06 11:11:43 +0530

    6th July _Multiplication of table 9 task

---
+++ src/ex_02_Java_Basic/Task_Multiplication_of_Table_9.java
@@ -0,0 +1,31 @@
+package ex_02_Java_Basic;
+
+public class Task_Multiplication_of_Table_9 {
+    public static void main(String[] args) {
+        int a = 9;
+
+        System.out.println("==== Multiplication of Table 9 =====");
+        System.out.printf("%d x %d = %d%n" , a, 1, a * 1);
+        System.out.printf("%d x %d = %d%n" , a, 2, a * 2);
+        System.out.printf("%d x %d = %d%n" , a, 3, a * 3);
+        System.out.printf("%d x %d = %d%n" , a, 4, a * 4);
+        System.out.printf("%d x %d = %d%n" , a, 5, a * 5);
+        System.out.printf("%d x %d = %d%n" , a, 6, a * 6);
+        System.out.printf("%d x %d = %d%n" , a, 7, a * 7);
+        System.out.printf("%d x %d = %d%n" , a, 8, a * 8);
+        System.out.printf("%d x %d = %d%n" , a, 9, a * 9);
+        System.out.printf("%d x %d = %d%n" , a, 10, a * 10);
+
+    //   Type 1 : System.out.printf("%d x 1 = %d \n" , a ,a*1);
+    //   Type 2 : System.out.printf("9 × %d = %d\n", 1, 9 * 1);
+//        Type 3 :
+//        int a1 = 9 ;
+//        int b1 = 1;
+//        System.out.printf("%d × %d = %d\n", a1, b1, a1 * b1);
+//        b1 = 2;
+//        System.out.printf("%d × %d = %d\n", a1, b1, a1 * b1);
+
+
+
+    }
+}
